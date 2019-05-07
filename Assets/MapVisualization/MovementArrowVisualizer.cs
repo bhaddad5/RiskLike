3033150a41e8a40489229b8283372d86746aab1f
Bert Haddad <bhaddad5@gmail.com>
2019-05-07 01:38:18 -0700

    Arrow vis working

---
+++ Assets/MapVisualization/MovementArrowVisualizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementArrowVisualizer : MonoBehaviour
+{
+	public GameObject Arrow;
+	public GameObject Line;
+
+	public void Setup(Vector3 from, Vector3 to)
+	{
+		Line.transform.position = from + new Vector3(0, .05f, 0);
+		Line.transform.up = (to - from);
+		Line.transform.localScale = new Vector3(1, (to - from).magnitude, 1);
+		Line.transform.eulerAngles = new Vector3(90f, Line.transform.eulerAngles.y, Line.transform.eulerAngles.z);
+		Arrow.transform.position = to + new Vector3(0, .05f, 0);
+		Arrow.transform.up = (to - from);
+		Arrow.transform.eulerAngles = new Vector3(90f, Arrow.transform.eulerAngles.y, Arrow.transform.eulerAngles.z);
+
+	}
+}
---
+++ Assets/MapVisualization/MovementPathVisualizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPathVisualizer : MonoBehaviour
+{
+	public MovementArrowVisualizer ArrowPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    var arrow1 = GameObject.Instantiate(ArrowPrefab);
+		arrow1.Setup(new Vector3(0, 0, 0), new Vector3(3, 0, 5));
+
+	    var arrow2 = GameObject.Instantiate(ArrowPrefab);
+	    arrow2.Setup(new Vector3(3, 0, 5), new Vector3(7, 0, 2));
+	}
+
+	public void VisualizePath(List<Vector3> path)
+	{
+
+	}
+}
